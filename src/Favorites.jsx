a0e4698eb510b4788d74d524c926de670f51cccf
Aravinth <aravinthramasamy93@gmail.com>
2025-10-06 17:21:37 +0530

    again name changed

--- src/Favorites.jsx
+++ src/Favorites.jsx
@@ -29,7 +29,7 @@ function Favorites({ favorites, removeFavorite }) {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {favorites.map((movie) => (
-            <MovieCard
+            <Moviecard
               key={movie.imdbID}
               movie={movie}
               isFav={true}
